00a0c3cdc1754ac8d4645cda90225b4b76992baf
morganfeeney <info@morganfeeney.com>
2026-05-01 11:38:17 +0100

    Use cn-ui

--- apps/shadcnpreset/components/shadcn-examples/dashboard/components/section-cards.tsx
+++ apps/shadcnpreset/components/shadcn-examples/dashboard/components/section-cards.tsx
@@ -1,4 +1,4 @@
-import { Badge } from "@/components/ui/badge";
+import { Badge } from "@/components/ui/badge"
 import {
   Card,
   CardAction,
@@ -6,12 +6,12 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { IconPlaceholder } from "@/components/icon-placeholder";
+} from "@/components/cn-ui/card"
+import { IconPlaceholder } from "@/components/icon-placeholder"
 
 export function SectionCards() {
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+    <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
       <Card className="@container/card">
         <CardHeader>
           <CardDescription>Total Revenue</CardDescription>
@@ -153,5 +153,5 @@ export function SectionCards() {
         </CardFooter>
       </Card>
     </div>
-  );
+  )
 }
