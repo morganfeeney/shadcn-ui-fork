af21917e5ff0573f63f3cdc5f62f66297ba5fd9b
morganfeeney <info@morganfeeney.com>
2026-05-06 08:07:56 +0100

    Fix mobile issue

--- apps/shadcnpreset/components/list-view.tsx
+++ apps/shadcnpreset/components/list-view.tsx
@@ -98,7 +98,7 @@ export function ListView({
 
   return (
     <section>
-      <ul className="grid grid-cols-[repeat(auto-fill,minmax(400px,1fr))] gap-6 gap-y-4">
+      <ul className="grid grid-cols-[repeat(auto-fill,minmax(min(100%,400px),1fr))] gap-6">
         {visibleItems.map((item) => (
           <li key={item.code}>
             <PresetStyleOverviewCard
